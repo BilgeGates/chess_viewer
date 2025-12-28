2bc139cf1ae123baad8a3c3b1b75ee0ac4404ca2
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2025-12-28 21:35:27 +0400

    feat: add initial PIECE_SETS data for chess pieces

---
+++ src/data/pieces.js
@@ -0,0 +1,10 @@
+export const PIECE_SETS = [
+  { id: "cburnett", name: "CBurnett (Default)", isCustom: false },
+  { id: "merida", name: "Merida", isCustom: false },
+  { id: "alpha", name: "Alpha", isCustom: false },
+  { id: "pirouetti", name: "Pirouetti", isCustom: false },
+  { id: "chessnut", name: "Chessnut", isCustom: false },
+  { id: "kosal", name: "Kosal", isCustom: false },
+  { id: "fresca", name: "Fresca", isCustom: false },
+  { id: "cardinal", name: "Cardinal", isCustom: false },
+];
