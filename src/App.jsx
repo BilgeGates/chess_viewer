6543a0f21893fc9f434571e4345fa73295a794ec
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2026-01-14 21:53:13 +0400

    refactor: clean up component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/layouts/Navbar";
-import AppRoutes from "./routes/Router";
+import Navbar from './components/layouts/Navbar';
+import Routes from './routes/Router';
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-900">
       <Navbar />
-      <AppRoutes />
+      <Routes />
     </div>
   );
 };
