5a5ffda010d0ebaccb1ca6f7ecba84bd31dafdc5
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2026-01-27 21:25:23 +0400

    refactor: update logger utility to use environment check for logging functions

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,13 +1,21 @@
-/**
- * Centralized logging utility
- */
-
-const isDev = process.env.NODE_ENV === 'development';
-
-const noop = () => {};
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const logger = {
-  log: isDev ? console.log.bind(console) : noop,
-  warn: isDev ? console.warn.bind(console) : noop,
-  error: console.error.bind(console)
+  log: (...args) => {
+    if (isDevelopment) {
+      console.log(...args);
+    }
+  },
+
+  warn: (...args) => {
+    if (isDevelopment) {
+      console.warn(...args);
+    }
+  },
+
+  error: (...args) => {
+    if (isDevelopment) {
+      console.error(...args);
+    }
+  }
 };
