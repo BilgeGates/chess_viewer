e054ee5f48f282157ffdc5c1c1916501e366780a
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2026-02-09 19:54:56 +0400

    fix: refactor webpack configuration to improve alias handling and structure

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,26 @@ module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src')
+    },
+
+    configure: (webpackConfig) => {
+      webpackConfig.resolve = {
+        ...webpackConfig.resolve,
+        extensions: ['.js', '.jsx', '.json'],
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+        alias: {
+          ...webpackConfig.resolve.alias,
+          '@': path.resolve(__dirname, 'src')
+        }
+      };
+
+      return webpackConfig;
+    }
+  },
+
+  style: {
+    postcss: {
+      mode: 'extends'
     }
   }
 };
