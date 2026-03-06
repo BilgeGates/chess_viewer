e3e1e72cfd0fef789ed1515a8656a62c8cd994b3
BilgeGates <huseynzadekhatai@gmail.com>
2026-03-06 23:57:30 +0400

    refactor: enhance documentation for Card component

--- src/components/ui/Card/Card.jsx
+++ src/components/ui/Card/Card.jsx
@@ -1,5 +1,15 @@
 import { getCardClasses } from '@/utils';
 
+/**
+ * Generic surface card wrapper.
+ * @param {Object} props
+ * @param {React.ReactNode} props.children - Card content
+ * @param {string} [props.className=''] - Additional Tailwind classes
+ * @param {boolean} [props.gradient=false] - Apply gradient background
+ * @param {boolean} [props.glass=false] - Apply glass-morphism style
+ * @param {'sm'|'md'|'lg'} [props.padding='md'] - Internal padding size
+ * @returns {JSX.Element}
+ */
 const Card = ({
   children,
   className = '',
