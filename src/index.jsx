d57afb552158a5c12f92ecf68f099fef0edd5539
BilgeGates <huseynzadekhatai@gmail.com>
2026-03-09 23:23:20 +0400

    refactor: remove unnecessary comments and whitespace in index.jsx for cleaner code

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './App';
-
 import './index.css';
-
-/**
- * Application entry point.
- * Renders the main App component wrapped in React Router.
- */
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <React.StrictMode>
+root.render(<React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>);
