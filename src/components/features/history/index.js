6bf83cb3955dbd6bfd04df27ca3e7829983693be
BilgeGates <huseynzadekhatai@gmail.com>
2026-03-09 23:23:10 +0400

    refactor: remove color utility functions from colorUtils.js to streamline codebase and reduce redundancy.

--- src/components/features/history/index.js
+++
@@ -1,3 +0,0 @@
-export { default as HistoryFilters } from './HistoryFilters';
-export { default as StatusBadge } from './StatusBadge';
-export { default as ConfirmationModal } from './ConfirmationModal';
