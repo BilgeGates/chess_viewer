51a1225ca61a1a0e620572fa07c4270d2fd4cfb6
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2026-05-09 22:03:52 +0400

    style: format code for better readability in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,9 @@ import App from './App';
 
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
